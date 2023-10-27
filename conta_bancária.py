bac71d1defe02df458ce07c028d8e0a37b827f03
Eduardo Dantas <eduardodanttas@icloud.com>
2023-10-27 00:41:18 -0300

    feat: add criar_usuario and filtrar_usuario to main

--- conta_bancária.py
+++ conta_bancária.py
@@ -4,9 +4,12 @@ def menu():
         [1] - Depositar
         [2] - Sacar
         [3] - Extrato
+        [4] - Novo Usuário
+        [5] - Nova conta
+        [6] - Listar Usuários
         [0] - Sair
 
-        => """
+        =>:  """
     return int(input(menu))
 
 def depositar_valor(valor, saldo, extrato):
@@ -45,12 +48,47 @@ def mostrar_extrato(saldo, extrato):
     print(f"\nSaldo: R$ {saldo:.2f}")
     print("==================================")
 
+def criar_usuario(usuarios):
+    cpf = input("Digite o seu CPF, apenas números: ")
+    usuario = filtrar_usuarios(cpf, usuarios)
+
+    if usuario:
+        print("Usuário já cadastrado")
+        return
+    
+    nome = input("Digite o seu nome: ")
+    data_de_nascimento = input("Digite sua data de nascimento no formato DD/MM/AAAA: ")
+    endereco_usuario = {
+        "rua": input("Digite a Rua do endereço com numero: "),
+        "bairro": input("Digite o Bairro do endereço: "),
+        "cidade": input("Digite a Cidade do endereço: "),
+        "estado": input("Digite o Estado do endereço: ")
+    }
+
+    novo_usuario = {
+        "nome": nome,
+        "data": data_de_nascimento,
+        "cpf": cpf,
+        "endereco": {},
+        "contas": []
+    }
+
+    novo_usuario["endereco"] = endereco_usuario
+    usuarios.append(novo_usuario)
+    
+def filtrar_usuarios(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
 def main():
+    AGENCIA = "0001"
     saldo = 0
     limite = 500
     extrato = ""
     numero_saque = 0
     LIMITE_SAQUE = 3
+    usuarios = []
+    contas = []
 
     while True:
         opcao = menu()
@@ -67,6 +105,22 @@ def main():
 
         elif opcao == 3:
             mostrar_extrato(saldo=saldo, extrato=extrato)
+
+        elif opcao == 4:
+            criar_usuario(usuarios)
+        
+        elif opcao == 5:
+            print
+
+        elif opcao == 6:
+            for usuario in usuarios:
+                print("Usuário:", usuario["nome"])
+                print("CPF:", usuario["cpf"])
+                print("Contas vinculadas:")
+            for conta in usuario["contas"]:
+                print("Agência:", conta["agencia"])
+                print("Número da Conta:", conta["numero_conta"])
+                print("--------------------")
 
         elif opcao == 0:
             break
