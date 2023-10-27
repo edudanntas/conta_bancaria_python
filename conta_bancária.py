5185985fd41cf9720f803361ff018f986579792e
Eduardo Dantas <eduardodanttas@icloud.com>
2023-10-26 23:55:42 -0300

    feat: add sacar_valor function to main

--- conta_bancária.py
+++ conta_bancária.py
@@ -15,6 +15,29 @@ def depositar_valor(valor, saldo, extrato):
         extrato += f"Depósito: R$ {valor:.2f}\n"
     return saldo, extrato
 
+def sacar_valor(valor, saldo, limite, extrato, limite_saque, numero_saque):
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saque = numero_saque >= limite_saque
+
+    if excedeu_saldo:
+        print("Saque não realizado! Você não possui saldo suficiente")
+
+    elif excedeu_limite:
+        print(
+            f"Saque não realizado! Valor informado ultrapassa o limite diário de: R$ {limite:.2f}")
+    elif excedeu_saque:
+        print("Saque não realizado! Seu limite diário de saques foi atingido")
+
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        numero_saque += 1
+
+    else:
+        print("Saque não realizado! Valor informado para saque inválido")
+    
+    return saldo, extrato, numero_saque
 
 def main():
     saldo = 0
@@ -32,29 +55,9 @@ def main():
             saldo, extrato = depositar_valor(valor=valor, saldo=saldo, extrato=extrato)
 
         elif opcao == 2:
-            valor_saque = float(input("Digite o valor que deseja sacar: "))
+            valor = float(input("Digite o valor que deseja sacar: "))
 
-            excedeu_saldo = valor_saque > saldo
-            excedeu_limite = valor_saque > limite
-            excedeu_saque = numero_saque >= LIMITE_SAQUE
-
-            if excedeu_saldo:
-                print("Saque não realizado! Você não possui saldo suficiente")
-
-            elif excedeu_limite:
-                print(
-                    f"Saque não realizado! Valor informado ultrapassa o limite diário de: R$ {limite:.2f}")
-
-            elif excedeu_saque:
-                print("Saque não realizado! Seu limite diário de saques foi atingido")
-
-            elif valor_saque > 0:
-                saldo -= valor_saque
-                extrato += f"Saque: R$ {valor_saque:.2f}\n"
-                numero_saque += 1
-
-            else:
-                print("Saque não realizado! Valor informado para saque inválido")
+            saldo, extrato, numero_saque = sacar_valor(valor=valor, saldo=saldo, limite=limite, extrato=extrato, limite_saque=LIMITE_SAQUE, numero_saque=numero_saque)
 
         elif opcao == 3:
             print("\n=============EXTRATO============")
