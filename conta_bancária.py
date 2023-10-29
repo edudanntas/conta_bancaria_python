68bf4a3ab20721bac4b6172880264fbace44c614
Eduardo Dantas <eduardodanttas@icloud.com>
2023-10-29 14:24:09 -0300

    feat: add Deposito class

--- conta_bancária.py
+++ conta_bancária.py
@@ -155,6 +155,21 @@ class Sacar(Transacao):
             conta.historico.adicionar_transacao(self)
 
 
+class Desposito(Transacao):
+    
+    def __init__(self, valor):
+        self._valor = valor
+    
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.depositar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
+
 
 def menu():
     menu = """
