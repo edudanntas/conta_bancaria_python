0fa18b2dc37a7914b40252c6734ed9470ca472a5
Eduardo Dantas <eduardodanttas@icloud.com>
2023-10-29 14:19:45 -0300

    feat: add PessoaFisica class

--- conta_bancária.py
+++ conta_bancária.py
@@ -11,6 +11,14 @@ class Cliente:
     def realizar_transacao(self, conta, transacao):
         transacao.registrar(conta)
 
+class PessoaFisica(Cliente):
+
+    def __init__(self, nome, cpf, data_nascimento):
+        super().__init__(endereco)
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nascimento = data_nascimento
+
 def menu():
     menu = """
 
