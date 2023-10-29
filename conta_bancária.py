8e970d271565ab3e10470fbfdca0af34224d237e
Eduardo Dantas <eduardodanttas@icloud.com>
2023-10-29 14:19:15 -0300

    feat: add Cliente class

--- conta_bancária.py
+++ conta_bancária.py
@@ -1,3 +1,16 @@
+
+class Cliente:
+
+    def __init__(self, endereco):
+        self._endereco = endereco
+        self._contas = []
+
+    def adicionar_conta(self, conta):
+        self._contas.append(conta)
+    
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+
 def menu():
     menu = """
 
